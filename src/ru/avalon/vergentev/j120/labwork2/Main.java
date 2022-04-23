840826562d5b59eed1ad19676b63be23923d1b39
vTikhon <tikhon.v@gmail.com>
2022-04-23 18:48:03 +0300

    добавил методов. Работаю над добавлением

--- src/ru/avalon/vergentev/j120/labwork2/Main.java
+++ src/ru/avalon/vergentev/j120/labwork2/Main.java
@@ -1,71 +1,41 @@
 package ru.avalon.vergentev.j120.labwork2;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         //>>>>>>>>>>>>>>>>>>>>>>>>>>>>ЗАДАНИЕ 1<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-        MyProperties myProperties2 = new MyProperties(); //согласно конструктору файл с заголовком, но с пустыми значениями
-//        myProperties.printFileInConsole();
-        myProperties2.myPut("key1", "value1");
-        myProperties2.myPut("key2", "value2");
-        myProperties2.myPut("key3", "value3");
-//        myProperties.printFileInConsole();
+        MyProperties myProperties = new MyProperties(); //согласно конструктору файл с заголовком, но с пустыми значениями
+//        myProperties.printFileDataInConsole(); //выводим значения всего файла на консоль
+
+        myProperties.myPut("key1", "value1");
+        myProperties.myPut("key2", "value2");
+        myProperties.myPut("key3", "value3");
+
+//        myProperties.myStore();
+//        myProperties.printFileDataInConsole();
+
 //        MyProperties myProperties2 = new MyProperties(new File("fileForTask1.txt")); //конструктор для считывания файла с данными
-//        myProperties2.printFileInConsole();
+//        myProperties2.printFileDataInConsole(); //выводим значения всего файла на консоль
+//        System.out.println('\n');
+//        System.out.println("The value of the key2 is:");
+//        System.out.println(myProperties2.myGet("key2")); //выводим значение ключа2
+//        myProperties2.myReplace("key2", "value44"); //изменяем значение ключа2
+//        System.out.println("The value of the key2 is:");
+//        System.out.println(myProperties2.myGet("key2")); //выводим новое значение ключа2
+//        myProperties2.myRemove("key2");
+//        System.out.println("The value of the key2 is:");
+//        System.out.println(myProperties2.myGet("key2")); //выводим новое значение ключа2
+//        System.out.println("Is the value of the key2 exists?");
+//        System.out.println(myProperties2.myExists("key2"));
+//        System.out.println("Is the value of the key3 exists?");
+//        System.out.println(myProperties2.myExists("key3"));
+//        System.out.println('\n');
+//        myProperties2.myStore(); //записываем в тот же файл новые значения
+//        myProperties2.myStoreInNewFile("D:/fileForTask1.txt"); //записываем в новый файл новые значения
+//        myProperties2.printFileDataInConsole(); //выводим значения всего файла на консоль
 
 
-//            Properties testProperties = new Properties();
-//            testProperties.put();
-
-
-
-
-
-
-//        try {
-//            DataTask1 dataTask1 = new DataTask1();
-//            dataTask1.printInConsole();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-
-//        //формируем произвольные данные, которые помещаем в коллекцию
-//        Map<String, String> dataHashMap = new HashMap<>();
-//        dataHashMap.put("key1", "value1");
-//        dataHashMap.put("key2", "value2");
-//        dataHashMap.put("key3", "value3");
-//        dataHashMap.put("key4", "value4");
-//        //перезаписываем все элементы коллекции в StringBuffer, которую потом запишем в файл
-//        StringBuffer data = new StringBuffer();
-//        for (Map i : dataHashMap) {
-//            data.append(i);
-//        }
-//        //определяем в какой файл мы будем записывать
-//        Task1 task1 = new Task1(new File("D:/Task2a-J120-VERGENTEV.txt"), data);
-//        task1.writer(); //записываем данные в файл
-//        System.out.println("Print data of file:");
-//        task1.printInConsole(); //выводим данные файла в консоль
-//        System.out.print('\n');
-//        System.out.println("Check is there set value in file:");
-//        System.out.println(task1.finderValue("value3"));
-//        System.out.print('\n');
-//        System.out.println("Change element in a file:");
-//        task1.finderValueAndRename("value3", "value27"); //меняем значение одно на другое
-
-
-        //формируем произвольные данные, которые помещаем в StringBuilder
-//        StringBuffer data = new StringBuffer();
-//        for (int i = 1; i < 10; i++) {
-//            data.append("value").append(i).append("=key").append(i).append('\n');
-//        }
-//        //определяем в какой файл мы будем записывать данные
-//        Task1 task1 = new Task1(new File("D:/Task2a-J120-VERGENTEV.txt"), data);
-//        System.out.println("Print data of file:");
-//        task1.writer();
-//        task1.printInConsole();
-//        System.out.print('\n');
-//        System.out.println("Check is there set value in file:");
-//        System.out.println(task1.finderValue("value3"));
 
 
 
--- src/ru/avalon/vergentev/j120/labwork2/MyProperties.java
+++ src/ru/avalon/vergentev/j120/labwork2/MyProperties.java
@@ -2,11 +2,14 @@ package ru.avalon.vergentev.j120.labwork2;
 import java.io.*;
 import java.util.*;
 
-//ЗАДАНИЕ 1 - СОЗДАЁМ КЛАСС АНАЛОГИЧНЫЙ БИБЛИОТЕЧНОМУ Properties (не используя Properties)
+//ЗАДАНИЕ 1 - СОЗДАЁМ КЛАСС MyProperties АНАЛОГИЧНЫЙ БИБЛИОТЕЧНОМУ Properties
 public class MyProperties {
     File file;
     FileReader fileReader;
     FileWriter fileWriter;
+    Map<String, String> map;
+    StringBuilder data;
+    String[] title;
 
 
     //CONSTRUCTORS
@@ -14,8 +17,8 @@ public class MyProperties {
         try {
             fileWriter = new FileWriter("fileForTask1.txt");
             fileWriter.append("#").append(new Date().toString()).append('\n');
-            fileWriter.append("\nkey1=value1").append("\nkey2=value2").append("\nkey3=value3");
             fileWriter.close();
+            map = new HashMap<>();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -27,16 +30,16 @@ public class MyProperties {
             if (!file.exists() || !file.canRead()) throw new SecurityException("File doesn't exist or can't be readable !!!");
             int dataExisting;
             fileReader = new FileReader(file);
-            StringBuilder temp = new StringBuilder();
+            data = new StringBuilder();
             while ((dataExisting = fileReader.read()) != -1) {
-                temp.append((char)dataExisting);
+                data.append((char)dataExisting);
             }
-            String[] temp2 = temp.toString().split("[\n]");  //запишем в массив каждую строку отдельно для того чтобы потом пропустить заголовок
-            Map<String, String> map = new HashMap<>();
-            for (int i = 2; i < temp2.length; i++) {   //пропускаем заголовок и пустую строку
-                new StringBuilder().append(temp2[i]);
-                String[] temp3 = temp2[i].split("[=]");
-                map.put(temp3[0], temp3[1]);    //помешаем ключ и значение в подходящую коллекцию HashMap
+            title = data.toString().split("[\n]");  //запишем в массив каждую строку отдельно для того чтобы потом пропустить или вернуть заголовок
+            map = new HashMap<>();
+            for (int i = 2; i < title.length; i++) {   //пропускаем заголовок и пустую строку
+                new StringBuilder().append(title[i]);
+                String[] temp = title[i].split("[=]");
+                map.put(temp[0], temp[1]);    //помешаем ключ и значение в подходящую коллекцию HashMap
             }
             fileReader.close();
         } catch (IOException e) {
@@ -45,7 +48,7 @@ public class MyProperties {
     }
 
     //METHODS
-    public void printFileInConsole () {
+    public void printFileDataInConsole () {
         if (!file.exists() || !file.canRead())  throw new SecurityException("File can't be readable");
         int dataInfo;
         try {
@@ -61,10 +64,59 @@ public class MyProperties {
 
     //записываем в файл произвольные данные
     public void myPut (String key, String value) {
-        if (!file.exists() || !file.canWrite())  throw new SecurityException("File can't be writable");
+        map.put(key, value);
+    }
+
+    public String myGet (String key) {
+        return map.get(key);
+    }
+
+    public void myReplace (String key, String value) {
+        if (map.get(key) != null) map.put(key, value);
+    }
+
+    public void myRemove (String key) {
+        map.remove(key);
+    }
+
+    public boolean myExists (String key) {
+        return map.get(key) != null;
+    }
+
+    public void myStore () {
+        if (!file.exists() || !file.canWrite())  throw new SecurityException("File can't be writable or doesn't exist !!!");
         try {
-            fileWriter = new FileWriter(file, true);
-            fileWriter.write('\n' + key + "=" + value);
+            System.out.println("i am crying");
+            data = new StringBuilder();
+            data.append(title[0]).append('\n').append(title[1]);
+            for (Object i : map.keySet()){
+                data.append('\n').append(i).append("=").append(map.get(i));
+        }
+            fileWriter = new FileWriter(file, false);
+            fileWriter.write(String.valueOf(data));
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void myStoreInNewFile (String url) {
+        if (url == null) {
+            assert false;
+            if (!url.isEmpty()) throw new IllegalArgumentException("URL can't be empty");
+        }
+        try {
+            file = new File(url);
+            if (!file.exists()) {
+                file.createNewFile();
+            };
+            data = new StringBuilder();
+            data.append(title[0]).append('\n').append(title[1]);
+            for (Object i : map.keySet()){
+                data.append('\n').append(i).append("=").append(map.get(i));
+            }
+            fileWriter = new FileWriter(file, false);
+            fileWriter.write(String.valueOf(data));
             fileWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
