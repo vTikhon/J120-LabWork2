77e159844ce0c0492cd080c07018779cf81905b4
vTikhon <tikhon.v@gmail.com>
2022-04-24 20:19:47 +0300

    cant replace string to int

--- src/ru/avalon/vergentev/j120/labwork2/Decripter.java
+++ src/ru/avalon/vergentev/j120/labwork2/Decripter.java
@@ -1,15 +1,17 @@
 package ru.avalon.vergentev.j120.labwork2;
 import java.io.*;
-import java.util.ArrayList;
+import java.sql.SQLOutput;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 public class Decripter {
     File file;
     FileReader fileReader;
     FileWriter fileWriter;
     StringBuilder data, dataWithoutComments;
-    String[] dataEachString, dataEachStringForPrint, dataEachStringForSet;
-    Map<String, Integer> map;
+    String[] dataEachString, dataEachStringForPrint, dataEachStringForSet, dataEachStringForSetEachFragment;
+    Properties map;
 
     public Decripter (File file) {
         try {
@@ -29,65 +31,87 @@ public class Decripter {
     }
 
     //METHODS
-    public void printFileDataInConsole () {
-        if (!file.exists() || !file.canRead())  throw new SecurityException("File can't be readable or doesn't exist !!!");
-        try {
-            int symbolExisting;
-            fileReader = new FileReader(file);
-            while ((symbolExisting = fileReader.read()) != -1) {
-                System.out.print((char)symbolExisting);
-            }
-            fileReader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+//    public void printFileDataInConsole () {
+//        if (!file.exists() || !file.canRead())  throw new SecurityException("File can't be readable or doesn't exist !!!");
+//        try {
+//            int symbolExisting;
+//            fileReader = new FileReader(file);
+//            while ((symbolExisting = fileReader.read()) != -1) {
+//                System.out.print((char)symbolExisting);
+//            }
+//            fileReader.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
-    public void printDecriptData () {
-        dataWithoutComments = new StringBuilder();
-        for (String i : dataEachString) {
-            if (!(i.charAt(0)=='#') && !(i.length() == 1)) {
-                dataWithoutComments.append(i).append('\n');
-            }
-        }
-        dataEachString = dataWithoutComments.toString().split("\n");
-        for (String i : dataEachString)
-        System.out.println(i);
-    }
-
-
-
-
-    public void dataDecripter () {
-        dataWithoutComments = new StringBuilder();
-        for (String i : dataEachString) {
-            if (!(i.charAt(0)=='#') && !(i.length() == 1)) {
-                dataWithoutComments.append(i).append('\n');
-            }
-        }
-        dataEachString = dataWithoutComments.toString().split("\n");
+//    public void printDecriptData () {
+//        dataWithoutComments = new StringBuilder();
+//        for (String i : dataEachString) {
+//            if (!(i.charAt(0)=='#') && !(i.length() == 1)) {
+//                dataWithoutComments.append(i).append('\n');
+//            }
+//        }
+//        dataEachString = dataWithoutComments.toString().split("\n");
 //        for (String i : dataEachString)
-//            System.out.println(i);
+//        System.out.println(i);
+//    }
 
+
+    //основной метод преобразования и анализа кода
+    public void dataDecripter () {
+        //убираем строки содержащие комментарии и пустые строки
+        dataWithoutComments = new StringBuilder();
+        for (String i : dataEachString) {
+            if (!(i.charAt(0)=='#') && !(i.length() == 1)) {
+                dataWithoutComments.append(i).append('\n');
+            }
+        }
+        //анализируем каждую из оставшихся строк на содержание операторов print или set
+        map = new Properties();
+        dataEachString = dataWithoutComments.toString().split("\n");
         for (String i : dataEachString) {
             if (i.contains("print")) {
-                dataEachStringForPrint = i.split("\"");
-                for (int j = 1; j < dataEachStringForPrint.length; j = j+2) {
-                    System.out.println(dataEachStringForPrint[j]);
-                }
-//                System.out.println(i.substring(i.indexOf("\""), i.indexOf("\"")));
+                printDecripter(i);
             }
             else if (i.contains("set")) {
-                dataEachStringForSet = i.split("\\$");
-                for (int j = 1; j < dataEachStringForSet.length; j = j+2) {
-                    System.out.println(dataEachStringForSet[j]);
+                setDecripter(i);
+            }
+        }
+
+
+
+        if (dataEachStringForSet[1].contains("+") || dataEachStringForSet[1].contains("-")) {
+            dataEachStringForSetEachFragment = dataEachStringForSet[1].split(" ");
+            for (String j : dataEachStringForSetEachFragment) {
+                for (Object k : map.keySet()) {
+                    if (j.equals(k)) {
+                        j = String.valueOf(map.get(k));
+                    }
                 }
             }
         }
+        for (Object k : map.keySet()) {
+            System.out.println(k);
+        }
+
+//        System.out.println(dataEachStringForSet[0] + " = " + map.get(dataEachStringForSet[0]));
     }
 
-    public String printDecripter (String string) {
+    //вспомогательный метод, который работает с оператором print
+    public String printDecripter (String i) {
+        dataEachStringForPrint = i.replace("print ", "").split("\"");
+        for (int j = 1; j < dataEachStringForPrint.length; j = j + 2) {
+//            System.out.println(dataEachStringForPrint[j]);
+        }
+        return i;
+    }
 
-        return string;
+    //вспомогательный метод, который работает с оператором set
+    public String setDecripter (String i) {
+        dataEachStringForSet = i.replace("set ", "").split(" = ");
+        map.put(dataEachStringForSet[0], dataEachStringForSet[1]);
+//        System.out.println(dataEachStringForSet[0] + " = " + map.get(dataEachStringForSet[0]));
+        return i;
     }
 }
--- src/ru/avalon/vergentev/j120/labwork2/Main.java
+++ src/ru/avalon/vergentev/j120/labwork2/Main.java
@@ -46,5 +46,6 @@ public class Main {
         decripter.dataDecripter();
 
 
+
     }
 }
